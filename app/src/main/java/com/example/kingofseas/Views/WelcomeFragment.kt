8ea953f9a5c86ef47c13d3693774072694aaf115
jibjoub <jb.despujol@gmail.com>
2022-10-26 13:02:18 +0200

    First 2 fragments with the navigation working

---
+++ app/src/main/java/com/example/kingofseas/Views/WelcomeFragment.kt
@@ -0,0 +1,41 @@
+package com.example.kingofseas.Views
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+import com.example.kingofseas.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [WelcomeFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class WelcomeFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_welcome, container, false)
+        val bt_credit : Button = view.findViewById(R.id.bt_credit)
+        bt_credit.setOnClickListener(){
+            findNavController().navigate(R.id.action_welcomeFragment_to_creditFragment)
+        }
+        // Inflate the layout for this fragment
+        return view
+    }
+}
